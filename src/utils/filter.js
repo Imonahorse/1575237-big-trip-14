97d509fc72cff23bdbb073d9bbb1b909e35ef0aa
Alexey <Awotjan@gmail.com>
2021-05-16 21:53:32 +0300

    8.2

--- src/utils/filter.js
+++ src/utils/filter.js
@@ -3,7 +3,7 @@ import {FilterType} from './constant.js';
 
 const filter = {
   [FilterType.EVERYTHING]: (events) => events.filter((event) => event.dateTo),
-  [FilterType.FUTURE]: (events) => events.filter((event) => isEventComing(event.dateFrom)),
+  [FilterType.FUTURE]: (events) => events.filter((event) => isEventComing(event.dateTo)),
   [FilterType.PAST]: (events) => events.filter((event) => isEventExpired(event.dateFrom)),
 };
 
